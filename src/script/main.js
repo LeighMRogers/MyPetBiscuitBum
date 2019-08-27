30380dd403b702ffb81cbe019ae48d53bbf50237
Leigh Rogers <leighrogers@Leighs-MacBook-Pro.local>
2019-08-27 17:37:49 -0500

    adding pet sounds exercise

--- src/script/main.js
+++ src/script/main.js
@@ -6,4 +6,18 @@ const biscuitBum = {
     species: "cat",
     nicknames: ["Biscuit Bum", "Stupid Cat"],
     age: 4,
+    meow: function () {
+        window.alert("Meow.");
+    },
+    purr: function () {
+        window.alert("PHRRRRRRR");
+    },
+    cry: function () {
+        window.alert("WAAAAA!!! WAAAAA!!!")
+    }
 }
+
+biscuitBum.meow()
+biscuitBum.purr()
+biscuitBum.cry()
+console.log(biscuitBum);
