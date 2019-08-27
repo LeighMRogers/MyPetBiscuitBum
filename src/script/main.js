e1a1223c855a2a077d454982e5eec41370d52de1
Leigh Rogers <leighrogers@Leighs-MacBook-Pro.local>
2019-08-27 17:29:17 -0500

    added pet object exercise

---
+++ src/script/main.js
@@ -0,0 +1,9 @@
+
+//Represent your pet: Create an object that represents your pet.
+
+biscuitBum = {
+    name: "Biscuit",
+    species: "cat",
+    nicknames: ["Biscuit Bum", "Stupid Cat"],
+    age: 4,
+}
